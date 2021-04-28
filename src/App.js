a64792f1a06ec13da2da2e4c910e1cf9e3e9c689
RgnDunes <singh.divyansh1802@gmail.com>
2021-04-29 03:02:13 +0530

    Added Banner, Added Product and Home component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
+
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/checkout">
-            <h1>Checkout</h1>
+            <Header />
           </Route>
           <Route path="/login">
             <h1>Login</h1>
           </Route>
           <Route path="/">
             <Header />
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt=""
+      />
+      <div className="home__row">
+        <Product
+          id="12321341"
+          title="The Lean Startup"
+          price={11.96}
+          rating={5}
+          image="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-config-201810?wid=1078&hei=624&fmt=jpeg&qlt=80&.v=1603332211000"
+        />
+        <Product
+          id="12321341"
+          title="The Lean Startup"
+          price={11.96}
+          rating={5}
+          image="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-config-201810?wid=1078&hei=624&fmt=jpeg&qlt=80&.v=1603332211000"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
